2f06522ba6c4702d00bce43721da3c7886930b35
Ajai <ajai@tickingminds.com>
2014-11-25 14:44:56 +0530

    added_login php and mail page 1

---
+++ login_submit.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+
+if(!isset( $_POST['email'], $_POST['password']))
+{
+$message = 'Please enter a valid username and password';
+}
+
+if(isset($_POST['email'], $_POST['password'])){
+$email=$_POST['email'];
+$pass=$_POST['password'];
+  $username = 'root';
+  $password = '';
+   $conn = new PDO("mysql:host=192.168.1.133;dbname=salary;",$username,$password);
+   $stmt ='SELECT email,password FROM users WHERE email="'.$email.'" AND password ="'.$pass.'"';
+   $stmt1=$conn->query($stmt);
+echo $stmt;
+   if($row = $stmt1->fetch((PDO::FETCH_ASSOC))){
+
+               header("Location: emp_1.html");
+           }
+
+    }
+
+?>
